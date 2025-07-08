f2ca12d744f0eee925c014bfe0f870fe0a779979
DL InfoTech <dl.infotech22@gmail.com>
2025-07-08 06:21:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <button class="inl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    className="h-10 w-10 text-primary"
+                    className="h-12 w-12 text-primary"
                   >
                     <path d="M12 2L2 7l10 5 10-5-10-5z"></path>
                     <path d="M2 17l10 5 10-5"></path>
